f8cbc0e427f19b452ca81c4b457a6c8b8c2c1627
Gabriel Teles <bilebile2020@gmail.com>
2025-09-18 17:06:33 -0300

    Creation of the /src folder for better organization.

---
+++ src/routes.js
@@ -0,0 +1,53 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { Router } from "express";
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const router = Router();
+
+router.post("/contact", async (req, res) => {
+    const { name, email, message } = req.body;
+
+    const cleanName = name.trim().replace(/\s+/g, " ");
+
+    if (cleanName === "" || email === "" || message === "") {
+        return res.status(400).json({ inputError: "Preencha todos os campos." });
+    }
+
+    if (cleanName.match(/[^\p{L} ]/ug) !== null) {
+        return res.status(400).json({ inputError: "Nome inválido! Apenas letras e espaços simples são permitidos." });
+    }
+
+    if (!email.match(/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/)) {
+        return res.status(400).json({ inputError: "E-mail inválido!" });
+    }
+
+    if (
+        message.length < 10 ||
+        message.length > 500 ||
+        !message.match(/^[\p{L}\d\s.,!?'"():;-]+$/u)
+    ) {
+        return res.status(400).json({ inputError: "Campo de texto inválido!" });
+    }
+
+    const msg = {
+        to: "gabrieltlssimoes@gmail.com",
+        from: 'gabrieltlssimoes@gmail.com',
+        replyTo: email,
+        subject: 'Contato - Gabriel Teles',
+        text: `Nome: ${cleanName}\nMessage: ${message}`,
+        html: `<h2>${cleanName}</h2>
+        <p>${message}</p>`,
+    }
+
+    try {
+        await sgMail.send(msg);
+        res.json({ message: "E-mail enviado com sucesso." });
+    } catch (error) {
+        res.status(500).json({ error: "E-mail não enviado." });
+    }
+});
+
+export { router };
