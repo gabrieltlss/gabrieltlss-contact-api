3504bd2e4119272e16ddd652e6af8946b14af808
Gabriel Teles <bilebile2020@gmail.com>
2025-11-25 14:54:26 -0300

    Modifica index.js para a fim de testar funcionamento do servidor

--- src/index.js
+++ src/index.js
@@ -12,5 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server initialized.`));
+console.log("Server iniciado");
